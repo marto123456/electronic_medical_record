2d916bc8c06172eebf05f12f1340627080c0b699
Ogbonna Martins <georgeenueshike@gmail.com>
2021-07-24 10:14:54 +0100

    Dashboard code for health worker and admin

--- application/views/backend/health_worker/list_patients.php
+++ application/views/backend/health_worker/list_patients.php
@@ -14,6 +14,8 @@
                                 <th>Gender</th>
                                 <th>Age</th>
                                 <th>BMI</th>
+                                <th>Created By</th>
+                                <th>Assigned To</th>
                                 <th>Options</th>
                             </tr>
                         </thead>
@@ -29,6 +31,20 @@
                                 <td><?= $patient['gender']; ?></td>
                                 <td><?= $patient['age']; ?></td>
                                 <td><?= $patient['bmi']; ?></td>
+                                <td>
+                                    <?php $hw = $this->session->userdata('health_worker_id');
+                                        $hwab = $this->db->get_where('health_worker', array('health_worker_id' => $hw))->row_array();
+                                        echo $hwab['fname'].' '. $hwab['lname'];
+                                    ?>
+
+                                </td>
+                                <td>
+                                    <?php $en = $this->db->get_where('encounter', array('user_id' => $patient['user_id']))->row_array()['send_to'];
+                                        $hw = $this->db->get_where('health_worker', array('health_worker_id' => $en))->row_array();
+                                        echo $hw['fname'].' '. $hw['lname'];
+                                    ?>
+
+                                </td>
                                 <td>
                                 	<a target="_blank" href="<?php echo base_url(); ?>health_worker/encounter/<?php echo $patient['user_id']; ?>" class="text-primary">Encounter</a> |
                                 	<a href="" class="text-success">Edit</a> |
